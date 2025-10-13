6b2eeaa57ddcd0e88b52b5d60d3ec4c178bfa711
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-10-13 15:09:04 +0700

    Решено задание 2

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+int main(void) {
+    double numbers[10];
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+    for (int i = 0 ; i <10 ; i++){
+        if (scanf("%lf", &numbers[i]) != 1) {
+            return 1;
+        }
+    }
+    for (int i = 9; i >= 0; i--) {
+        printf("%.0f", numbers[i]);
+        if (i > 0) {
+            printf(" ");
+        }
+    }
+    printf("\n");
     return 0;
 }
