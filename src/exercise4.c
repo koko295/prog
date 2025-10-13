d476f667a48e876bc14c4a31fe77dd4f110ef3cd
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-10-13 15:30:06 +0700

    Решено задание 4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc, char *argv[]) {
+    double numbers[10];
+    double X;
+    int K;
+    int n = 10;
+
+    X = atof(argv[1]);
+    K = atoi(argv[2]);
+
+    for (int i = 0; i < n; i++) {
+        if (scanf("%lf", &numbers[i]) != 1) {
+            return 1;
+        }
+    }
+
+    for (int i = n - 1; i > K; i--) {
+        numbers[i] = numbers[i - 1];
+    }
+
+    numbers[K] = X;
+
+    for (int i = 0; i < n; i++) {
+        printf("%.3f", numbers[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
     return 0;
 }
