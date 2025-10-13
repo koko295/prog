ac5bbf80d7ee694962e2f47c0d31e0f9c2b18866
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-10-13 14:36:46 +0700

    test commit

---
+++ src/exercise1 copy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    double numbers[10];
+    double sum = 0.0;
+
+    for (int i = 0; i < 10; i++){
+        if (scanf("%lf", &numbers[i]) != 1) {
+            return 1;
+        }
+        sum += numbers[i];
+    }
+
+    double srdarif = sum / 10.0;
+    printf("%.3f\n", srdarif);
+
+    return 0;
+}
