61d4af620c1f4fb691a568edb78b8e2fcca463bd
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-10-13 15:26:35 +0700

    Решено задание 3

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc, char *argv[]) {
+    double numbers[10];
+
+    int K = atoi(argv[1]);
+
+    int n = 10;
+    K = K % n;
+    if (K < 0) {
+        K += n;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (scanf("%lf", &numbers[i]) != 1) {
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int index = (i - K + n) % n;
+        printf("%.3f", numbers[index]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
     return 0;
 }
