faace8eb494e8f102601a83d8093c6bb110e56fa
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-10-13 13:46:47 +0700

    Решено задание 1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+int main(void) {
+    double numbers[10];
+    double sum = 0.0;
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+    for (int i = 0; i < 10; i++){
+        if (scanf("%lf", &numbers[i]) != 1) {
+            return 1;
+        }
+        sum += numbers[i];
+    }
+
+    double srdarif = sum / 10.0;
+    printf("%.2f\n", srdarif);
+
     return 0;
 }
