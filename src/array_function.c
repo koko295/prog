c35306b24869ca5ae78f20cac53f0f77a991ae03
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-10-20 19:50:28 +0700

    Сделано задание хомеворк5

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,6 @@
-#include <stddef.h>
-#include "array_function.h"
+#include "../include/array_function.h"
 
-// 1. Объединение двух массивов в порядке src1 + src2
-void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+void join_int_arrays(const int* src1, size_t size_src1, const int* src2, size_t size_src2, int* dest) {
     for (size_t i = 0; i < size_src1; ++i) {
         dest[i] = src1[i];
     }
@@ -11,11 +9,10 @@ void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, i
     }
 }
 
-// 2. Слияние двух ОТСОРТИРОВАННЫХ массивов в один ОТСОРТИРОВАННЫЙ
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    size_t i = 0; // индекс в src1
-    size_t j = 0; // индекс в src2
-    size_t k = 0; // индекс в dest
+void join_and_sort_int_arrays(const int* src1, size_t size_src1, const int* src2, size_t size_src2, int* dest) {
+    size_t i = 0;
+    size_t j = 0;
+    size_t k = 0;
 
     while (i < size_src1 && j < size_src2) {
         if (src1[i] <= src2[j]) {
@@ -29,10 +26,7 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
     while (j < size_src2) dest[k++] = src2[j++];
 }
 
-// 3. Нахождение минимального и максимального элемента массива
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
-    if (size == 0) return; // защита от пустого массива
-
+void get_min_and_max_from_int_array(const int* src, size_t size, int* min, int* max) {
     int cur_min = src[0];
     int cur_max = src[0];
 
